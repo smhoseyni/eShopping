b644a12d1f9d1225766401b7c3379020a455a1ca
smhoseyni <smhoseyni@hotmail.com>
2024-02-05 10:00:14 +0330

    adding projects

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 namespace Catalog.API
 {
     public class Program
@@ -21,12 +23,25 @@ namespace Catalog.API
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
 
+            app.UseRouting();
+            app.UseStaticFiles();
+
             app.UseAuthorization();
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                   // ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
 
             app.MapControllers();
 
